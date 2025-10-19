72ca7b451d6ebe23546d887fb3fe85669fcdbf7c
Joshua Relatorres <49444369+RealJorres@users.noreply.github.com>
2025-10-19 16:51:16 +0800

    Update redirect URL to absolute path

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function Redirect(){
     var checkbox = document.getElementById("agree");
 
     if ( checkbox.checked){
-        window.location.href = "login.html";
+        window.location.href = "/login.html";
     }
     else{
         Swal.fire({
